b9d7fd41fed16835a838e1fd35302d9ff52f5a70
chavanjay892 <chavanjay892@gmail.com>
2024-12-29 08:05:43 +0530

    task no:1234

---
+++ Bank/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] a) {
+		BankService b = new BankService();
+		b.deposite("john", 12345, 10000);
+		b.withdrawal("john1", 12345, 10000, 1234);
+
+
+	}
+
+}
